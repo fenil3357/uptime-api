d85d8be43a7d8200e622d6f101e310fb87fbbfb2
Bacancy <fenil3357@gmail.com>
2025-02-04 13:34:09 +0530

    feat: get single user api

---
+++ src/controllers/user/user.controller.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Response } from "express";
+
+import { CustomError } from "../../helper/errors/custom-errors.js";
+import { httpStatusCodes } from "../../constant/httpStatus/httpStatusCodes.constants.js";
+import { IRequestWithUser } from "../../types/utils.interface.js";
+import { getOneUser } from "../../services/database/user/user.service.js";
+import { handleResponse } from "../../helper/response/handleResponse.js";
+
+export const getOneUserController = async (req: IRequestWithUser, res: Response, next: NextFunction): Promise<any> => {
+  try {
+    const user = req?.user;
+
+    const userData = await getOneUser({
+      id: user?.id
+    }, {
+      id: true,
+      email: true,
+      name: true,
+      monitors: true,
+      avatar: true
+    });
+
+    return handleResponse(
+      res,
+      {
+        message: 'User data fetched successfully.',
+        data: userData
+      }
+    );
+  } catch (error: any) {
+    console.log("🚀 ~ getUserById ~ error:", error?.message || error, error?.statusCode)
+    return next(
+      new CustomError(
+        error?.message || 'Something went wrong, please try again.',
+        error?.statusCode || httpStatusCodes['Internal Server Error']
+      )
+    )
+  }
+}
