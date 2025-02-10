25ce0b2559c03d9ac72fdf1f2ab6623842d77d0d
Bacancy <fenil3357@gmail.com>
2025-02-10 17:58:08 +0530

    feat: recent report failures monitor removal

--- src/services/cron/cron.service.ts
+++ src/services/cron/cron.service.ts
@@ -1,19 +1,24 @@
 import * as cron from 'node-cron'
 
 import { CRON_JOB_TYPES, CRON_JOBS } from '../../constant/cron/cron.constants.js'
-import { regularMonitorCheckJob } from '../bull/jobs/bull-jobs.service.js';
-import { serverHealthCheckJob } from '../server-health/jobs/server-health.jobs.js';
+import { recentFailureMonitorsRemovalJob, regularMonitorCheckJob } from '../bull/jobs/bull-jobs.service.js';
+import { serverHealthCheckJob } from '../bull/jobs/bull-jobs.service.js';
 
 export const scheduleCronJob = async (cronJobType: CRON_JOB_TYPES) => {
   try {
     switch (cronJobType) {
       case CRON_JOBS.REGULAR_MONITOR_CHECK: {
-        cron.schedule('*/1 * * * *', regularMonitorCheckJob)
+        cron.schedule('*/15 * * * *', regularMonitorCheckJob)
         break;
       }
 
       case CRON_JOBS.REGULAR_SERVER_HEALTH_CHECK: {
         cron.schedule(`*/10 * * * *`, serverHealthCheckJob);
+        break;
+      }
+
+      case CRON_JOBS.RECENT_ERROR_REPORTS_MONITOR_REMOVAL: {
+        cron.schedule(`*/45 * * * *`, recentFailureMonitorsRemovalJob);
         break;
       }
 
